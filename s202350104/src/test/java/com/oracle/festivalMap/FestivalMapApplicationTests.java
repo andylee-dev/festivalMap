6d7eb933ba06d5264f0d749be056d0ae995f71d7
minyong <minyong_e89@naver.com>
2023-10-26 22:20:26 +0900

    레포지토리명 변경

---
+++ s202350104/src/test/java/com/oracle/festivalMap/FestivalMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.festivalMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
