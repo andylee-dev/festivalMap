6d24470bd94372ecb61275f58f20c48b7d0314e9
minyong <minyong_e89@naver.com>
2023-10-26 22:35:32 +0900

    package name, application name 추가 변경(Test folder)

--- s202350104/src/test/java/com/oracle/s202350104/S202350104ApplicationTests.java
+++ s202350104/src/test/java/com/oracle/s202350104/S202350104ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.oracle.festivalMap;
+package com.oracle.s202350104;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FestivalMapApplicationTests {
+class S202350104ApplicationTests {
 
 	@Test
 	void contextLoads() {
