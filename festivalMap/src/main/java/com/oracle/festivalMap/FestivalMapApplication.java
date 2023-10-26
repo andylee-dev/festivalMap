d93c54716561e2e1fa3e222d294bd11a633c548d
admin <admin@DESKTOP-N4CPCN7>
2023-10-26 14:45:38 +0900

    최초 커밋

---
+++ festivalMap/src/main/java/com/oracle/festivalMap/FestivalMapApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.festivalMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalMapApplication.class, args);
+	}
+
+}
