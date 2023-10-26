11df98587507d5908914873b29c9d6fa4f8df731
minyong <minyong_e89@naver.com>
2023-10-26 22:32:32 +0900

    package name, application name 변경

--- s202350104/src/main/java/com/oracle/s202350104/S202350104Application.java
+++ s202350104/src/main/java/com/oracle/s202350104/S202350104Application.java
@@ -1,16 +1,16 @@
 /**
  * @author 이규현,
  */
-package com.oracle.festivalMap;
+package com.oracle.s202350104;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FestivalMapApplication {
+public class S202350104Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FestivalMapApplication.class, args);
+		SpringApplication.run(S202350104Application.class, args);
 	}
 
 }
