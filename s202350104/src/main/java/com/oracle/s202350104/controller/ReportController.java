921bfc1431cec4cfc1840a310aae5dd68837963e
songhwanjung <144098815+songhwanjung@users.noreply.github.com>
2023-10-27 14:45:07 +0900

    aa

--- s202350104/src/main/java/com/oracle/s202350104/controller/ReportController.java
+++ s202350104/src/main/java/com/oracle/s202350104/controller/ReportController.java
@@ -1,6 +1,7 @@
 package com.oracle.s202350104.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,4 +11,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ReportController {
 
+	@RequestMapping(value = "/report")
+	public String experience() {
+		return "report/reportList";
+	}
+	
+	@RequestMapping(value = "/report/detail")
+	public String experienceDetatil() {
+		return "report/reportDetail";
+	}
 }
