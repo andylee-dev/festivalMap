285b038d31ec7aef564d025713a56fff8a744776
skdhfl <skdhfl@gmail.com>
2023-10-27 15:56:05 +0900

    festivals dto

---
+++ s202350104/src/main/java/com/oracle/s202350104/model/Festivals.java
@@ -0,0 +1,19 @@
+package com.oracle.s202350104.model;
+
+import lombok.Data;
+
+@Data
+public class Festivals {
+	private int content_id;
+	private String hours;
+	private String start_date;
+	private String end_date;
+	private String sponsor;
+	private String eventplace;
+	private String cost;
+	private String overview;
+	private String is_parking;
+	private String is_stroller;
+	private String is_wheelchair;
+	private String is_restroom;
+}
