d748d5160d19c979d77db1accf79af8a8aea65b0
andylee-dev <andylee.devknight@gmail.com>
2023-10-27 11:19:06 +0900

    [feat] change home route

--- s202350104/src/main/java/com/oracle/s202350104/controller/ContentController.java
+++ s202350104/src/main/java/com/oracle/s202350104/controller/ContentController.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ContentController {
 
-	@RequestMapping(value = "/home")
+	@RequestMapping(value = "/")
 	public String home() {
 		return "home";
 	}
