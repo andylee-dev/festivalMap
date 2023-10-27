c93522062824f4d5d9651dda08844b8f936d9868
andylee-dev <andylee.devknight@gmail.com>
2023-10-27 15:27:48 +0900

    [draft] init content model

---
+++ s202350104/src/main/java/com/oracle/s202350104/model/Content.java
@@ -0,0 +1,19 @@
+package com.oracle.s202350104.model;
+
+import lombok.Data;
+
+@Data
+public class Content {
+	private int id;
+	private int area;
+	private int sigungu;
+	private int big_code;
+	private int small_code;
+	private String title;
+	private String content;
+	private String status;
+	private String img1;
+	private String img2;
+	private String img3;
+	private String address;
+}
