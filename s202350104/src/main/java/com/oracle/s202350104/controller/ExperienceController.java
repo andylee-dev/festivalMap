d43d060be3693a410b85f4f855809ebaf1b09b4e
songhwanjung <144098815+songhwanjung@users.noreply.github.com>
2023-10-27 14:15:55 +0900

    add Report, Experience Controller

---
+++ s202350104/src/main/java/com/oracle/s202350104/controller/ExperienceController.java
@@ -0,0 +1,14 @@
+package com.oracle.s202350104.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class ExperienceController {
+
+
+}
---
+++ s202350104/src/main/java/com/oracle/s202350104/controller/ReportController.java
@@ -0,0 +1,13 @@
+package com.oracle.s202350104.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class ReportController {
+
+}
