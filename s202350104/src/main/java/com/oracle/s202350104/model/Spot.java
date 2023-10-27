00a6b178461d7cb5a7dcf2e32000349d10be0496
ehgml14 <144099275+ehgml14@users.noreply.github.com>
2023-10-27 15:44:24 +0900

    .

---
+++ s202350104/src/main/java/com/oracle/s202350104/model/Spot.java
@@ -0,0 +1,18 @@
+package com.oracle.s202350104.model;
+
+import lombok.Data;
+
+@Data
+public class Spot {
+	private int 	contentId;
+	private String	openingHours;
+	private String	restDate;
+	private String	entranceFee;
+	private String	isRestroom;
+	private String	isWheelchair;
+	private String	isStroller;
+	private String	isPet;
+	private String	isParking;
+	
+
+}
