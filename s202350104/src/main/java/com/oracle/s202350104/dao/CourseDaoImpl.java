fcc26d51f1cbf297123a59e62107f804f6ba4e8a
namonamonamo <144099069+namonamonamo@users.noreply.github.com>
2023-10-27 17:52:33 +0900

    a

--- s202350104/src/main/java/com/oracle/s202350104/dao/CourseDaoImpl.java
+++ s202350104/src/main/java/com/oracle/s202350104/dao/CourseDaoImpl.java
@@ -3,11 +3,26 @@ package com.oracle.s202350104.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import com.oracle.s202350104.model.Course;
+
 import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
-public class CourseDaoImpl {
+public class CourseDaoImpl implements CourseDao {
 
 	private final SqlSession session;
+
+	@Override
+	public int courseCount(Course course) {
+		int courseCount = 0;
+		System.out.println("CourseDaoImpl courseCount start...");
+		try {
+			courseCount = session.selectOne("courseCount");
+			System.out.println("CourseDaoImpl courseCount courseCount ->" + courseCount);
+		} catch (Exception e) {
+			System.out.println("CourseDaoImpl courseCount e.getMessage() ->" + e.getMessage());
+		}
+		return courseCount;
+	}
 }
