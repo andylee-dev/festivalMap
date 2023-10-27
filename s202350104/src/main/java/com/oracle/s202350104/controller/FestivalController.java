48d51ac020423df4b7931416c075feb44dfab9f7
skdhfl <skdhfl@gmail.com>
2023-10-27 14:19:06 +0900

    festival Controller

--- s202350104/src/main/java/com/oracle/s202350104/controller/FestivalController.java
+++ s202350104/src/main/java/com/oracle/s202350104/controller/FestivalController.java
@@ -1,5 +1,38 @@
 package com.oracle.s202350104.controller;
 
-public class FestivalController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class FestivalController {
+	
+	@RequestMapping(value = "/festival")
+	public String festival() {
+		return "festival/festivalList";
+	}
+	
+	@RequestMapping(value = "/festival/detail")
+	public String festivalDetail() {
+		return "festival/festivalDetail";
+	}
+	
+	@RequestMapping(value = "/festival/recommendation")
+	public String festivalRecommendation() {
+		return "festival/festivalRecommendation";
+	}
+	
+	@RequestMapping(value = "/festival/test")
+	public String festivalTest() {
+		return "festival/festivalTest";
+	}
+	
+	@RequestMapping(value = "/festival/calendar")
+	public String festivalCalendar() {
+		return "festival/festivalCalendar";
+	}
 }
