3e3f255f7fb126f8cf3456527196bc526e4b1c1d
namonamonamo <144099069+namonamonamo@users.noreply.github.com>
2023-10-27 15:33:33 +0900

    course model add

---
+++ s202350104/src/main/java/com/oracle/s202350104/model/Course.java
@@ -0,0 +1,12 @@
+package com.oracle.s202350104.model;
+
+public class Course {
+	
+	private int		id;
+	private int		big_code;
+	private int		small_code;
+	private String  distance;
+	private String  course_info;
+	private String  time;
+	private String  schedule;
+}
