1e9b2f13d390c050a139878cd81ea53ac4a2245b
andylee-dev <andylee.devknight@gmail.com>
2023-10-30 16:59:51 +0900

    [draft] init admin content

--- s202350104/src/main/java/com/oracle/s202350104/controller/AdminController.java
+++ s202350104/src/main/java/com/oracle/s202350104/controller/AdminController.java
@@ -9,11 +9,45 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping(value = "/admin")
 public class AdminController {
-
-	@RequestMapping(value = "/admin")
+	
+	@RequestMapping(value = "")
 	public String home() {
 		return "admin/index";
 	}
 
+	@RequestMapping(value = "userList")
+	public String userList() {
+		return "admin/user/userList";
+	}
+
+	@RequestMapping(value = "bizUserList")
+	public String bizUserList() {
+		return "admin/user/bizUserList";
+	}
+
+	@RequestMapping(value = "festival")
+	public String festival() {
+		return "admin/content/festival";
+	}
+
+	@RequestMapping(value = "experience")
+	public String experience() {
+		return "admin/content/experience";
+	}
+	@RequestMapping(value = "restaurant")
+	public String restaurant() {
+		return "admin/content/restaurant";
+	}
+	@RequestMapping(value = "spot")
+	public String spot() {
+		return "admin/content/spot";
+	}
+	@RequestMapping(value = "accomodation")
+	public String accomodation() {
+		return "admin/content/accomodation";
+	}
+	
+	
 }
