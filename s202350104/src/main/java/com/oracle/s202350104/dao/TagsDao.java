da7e3082536e041a36e4a60793b0f71b38fa3b31
skdhfl <skdhfl@gmail.com>
2023-10-30 21:30:48 +0900

    tags update

---
+++ s202350104/src/main/java/com/oracle/s202350104/dao/TagsDao.java
@@ -0,0 +1,10 @@
+package com.oracle.s202350104.dao;
+
+import java.util.List;
+
+import com.oracle.s202350104.model.Tags;
+
+public interface TagsDao {
+	List<Tags> listTags();
+//	int        insertUserTags();
+}
