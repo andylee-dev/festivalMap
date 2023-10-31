54c66d4156cccec6eaa4884f0af70069585e8511
andylee-dev <andylee.devknight@gmail.com>
2023-10-31 12:29:35 +0900

    [feat] add DTO

---
+++ s202350104/src/main/java/com/oracle/s202350104/model/Areas.java
@@ -0,0 +1,12 @@
+package com.oracle.s202350104.model;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Areas {
+	private int area;
+	private int sigungu;
+	private String content;
+}
---
+++ s202350104/src/main/java/com/oracle/s202350104/model/CommonCodes.java
@@ -0,0 +1,10 @@
+package com.oracle.s202350104.model;
+
+import lombok.Data;
+
+@Data
+public class CommonCodes {
+	private int big_code;
+	private int small_code;
+	private String content;
+}
