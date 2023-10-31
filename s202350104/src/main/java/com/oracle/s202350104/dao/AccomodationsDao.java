3526760ffe2d4d5081cac351c4dedcbf37a2bff7
kabigonV2 <pokmon1221@gmail.com>
2023-10-31 14:12:15 +0900

    Accomodation update

--- s202350104/src/main/java/com/oracle/s202350104/dao/AccomodationsDao.java
+++ s202350104/src/main/java/com/oracle/s202350104/dao/AccomodationsDao.java
@@ -3,10 +3,15 @@ package com.oracle.s202350104.dao;
 import java.util.List;
 
 import com.oracle.s202350104.model.Accomodations;
+import com.oracle.s202350104.model.AccomodationsContent;
 
 public interface AccomodationsDao {
 
-	List<Accomodations>        listAccomodations(Accomodations accomodations);
+	List<Accomodations>        listAccomodations();
+
+	AccomodationsContent       detailAccomodations(int content_id);
+
+	
 
 
 
