d8b0a0c49cd7fa9d3b32dc99536c9c09f3153bcd
andylee-dev <andylee.devknight@gmail.com>
2023-11-03 09:21:13 +0900

    [feat] Add Auth feature

--- s202350104/src/main/java/com/oracle/s202350104/configuration/SecurityConfig.java
+++ s202350104/src/main/java/com/oracle/s202350104/configuration/SecurityConfig.java
@@ -2,44 +2,30 @@ package com.oracle.s202350104.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
- * @brief 인증 방식 두 개 사용해보기.
+ * TODO : 인증 방식 두 개 사용해보기.
  * - 관리자는 Server기반 인증
  * - 일반 유저는 Token기반 인증
  */
-
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
-public class SecurityConfig  {
-	
-	/**
-	 * @see Enum 타입 정의.
-	 */
-	public enum UserRole {
-	    ADMIN(1,"Admin"),
-	    BIZ(2,"Biz"),
-	    USER(3,"General");
+@Slf4j
+public class SecurityConfig {
 
-	    private int code;
-	    private String label;
+	private  CustomAuthenticationProvider authProvider;
 
-	    private UserRole(int code, String label) {
-	    	this.code = code;
-	        this.label = label;
-	    }
-	    public String getLabel() {
-	        return this.label;
-	    }
-	    public int getCode() {
-	        return this.code;
-	    }
-	}
 	/**
 	 * @see 시큐리티 무시할 경로 설정. static, h2데이터 확인하기 위한 h2-console.
 	 */
@@ -50,20 +36,45 @@ public class SecurityConfig  {
     }
 	
 	@Bean
-	public BCryptPasswordEncoder  encodePwd() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	@Bean
 	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception  {
+		// TODO: CSRF Study
 		http.csrf().disable();
-		http.authorizeRequests()
-			.antMatchers("/**").permitAll()
-//			.antMatchers("/admin/**").hasRole(UserRole.ADMIN.getLabel())
-//			.antMatchers("/my/**").hasRole(UserRole.ADMIN.getLabel())
-			.anyRequest().authenticated();
-		
+
+		// Role에 따른 Request권한 설정.
+		http.authorizeHttpRequests((requests) -> requests
+				/* 개발단계에서는 역할에 따른 접근제한 해제.
+				.antMatchers("/admin/**").hasRole(Role.ADMIN.getValue())
+				.antMatchers("/user/myPage/**").hasRole(Role.USER.getValue())
+				.antMatchers("/user/bizPage").hasRole(Role.BIZ.getValue())
+				*/
+				.antMatchers("/**").permitAll()
+				.anyRequest().authenticated()
+			);
+		// Login 설정 
+		http.formLogin((form) -> form
+				.loginPage("/login")
+				.loginProcessingUrl("/auth")
+				.usernameParameter("username")	// login에 필요한 id값 설정 (default는 username)
+                .passwordParameter("password")	// login에 필요한 password 값  (default password)
+				.permitAll()
+				.defaultSuccessUrl("/")
+				.successHandler(new CustomAuthenticationSuccessHandler())
+			);
+		// Logout 설정.
+		http.logout((logout) -> logout
+				.permitAll()
+				.logoutSuccessUrl("/")
+				.invalidateHttpSession(true) //세션 날리기
+			);	
+		// Authentication Provider 등록.
+		http.authenticationProvider(authProvider);
 		return http.build();
-		
+	}
+
+	@Bean
+	AuthenticationManager authenticationManager(
+		AuthenticationConfiguration authenticationConfiguration
+	) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 }
