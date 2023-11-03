e8e722a7cae96eacac7a2d3ff7d6ad77fce7613e
andylee-dev <andylee.devknight@gmail.com>
2023-11-03 09:13:36 +0900

    [fix] split Crypt feature as a AppConfig.java

---
+++ s202350104/src/main/java/com/oracle/s202350104/configuration/AppConfig.java
@@ -0,0 +1,20 @@
+package com.oracle.s202350104.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @biref UserService와 AuthProvider 양쪽에서 사용해야하기 때문에 순환참조로 인한 분리.
+ */
+@Configuration
+public class AppConfig {
+	@Bean
+	public BCryptPasswordEncoder  encodePwd() {
+		return new BCryptPasswordEncoder();
+	}	
+	
+}
