6b26239e68b357059bcc839dada1713137c2b655
andylee-dev <andylee.devknight@gmail.com>
2023-11-06 14:54:02 +0900

    [fix] update user feature

--- s202350104/src/main/java/com/oracle/s202350104/dao/UserDao.java
+++ s202350104/src/main/java/com/oracle/s202350104/dao/UserDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.oracle.s202350104.model.Users;
 
 public interface UserDao {
-	List<Users> listUsers();
+	List<Users> listSearchUsers(int small_code);
 	int insertUser(Users user);
 	Users getUserByEmail(String email);
 	Users getUserById(int id);
--- s202350104/src/main/java/com/oracle/s202350104/dao/UserDaoImpl.java
+++ s202350104/src/main/java/com/oracle/s202350104/dao/UserDaoImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import com.oracle.s202350104.configuration.Role;
 import com.oracle.s202350104.model.FestivalsContent;
 import com.oracle.s202350104.model.Users;
 
@@ -19,14 +20,8 @@ public class UserDaoImpl implements UserDao {
 	private final SqlSession session;
 	
 	@Override
-	public List<Users> listUsers() {
-		List<Users> listUsers = null;
-		try {
-			listUsers = session.selectList("userListAll");
-			log.info("listUsers() => " + listUsers.size());
-		} catch (Exception e) {
-			log.info("UserDaoImple listUsers() => " + e.getMessage());
-		}
+	public List<Users> listSearchUsers(int small_code ) {
+		List<Users> listUsers = session.selectList("userSearchList",small_code);
 		return listUsers;
 	}
 
