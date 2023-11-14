8a0356fbe23bbf0acd84057e23eafa1d5b7667af
kabigonV2 <pokmon1221@gmail.com>
2023-11-14 10:02:13 +0900

    kakao login update

--- s202350104/src/main/java/com/oracle/s202350104/service/user/oAuthService.java
+++ s202350104/src/main/java/com/oracle/s202350104/service/user/oAuthService.java
@@ -8,9 +8,12 @@ import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.oracle.s202350104.controller.kakaoOauth;
+import com.oracle.s202350104.model.Users;
 
 @Service
 public class oAuthService {
+	
+	/* private final UserRepository userRepository; */
 	
 	private static KakaoUserInfoDto getKakaoUserInfoDto(String code) throws JsonProcessingException {
 		ResponseEntity<String> accessTokenResponse = kakaoOauth.requestAccessToken(code);
@@ -22,8 +25,21 @@ public class oAuthService {
 
 	public static ResponseEntity<String> kakaoLogin(String code) throws IOException {
 		KakaoUserInfoDto kakaoUser = getKakaoUserInfoDto(code);
-		
+		/*
+		 * if(!userRepository.existsByEmail(kakaoUser.getEmail())) {
+		 * userRepository.save( User.builder() .email(kakaoUser.getmail())
+		 * .nickName(kakaoUser.getName()) .password("kakao") .id(kakaoUser.getName() +
+		 * LocalDateTime.now()) .build() );
+		 * 
+		 * return getSingleResult(userRepository.findByEmail( kakaoUser.getEmail()
+		 * ).orElseThrow(CEmailSignFailedException::new));
+		 * 
+		 * } return getSingleResult(userRepository.findByEmail( kakaoUser.getEmail()
+		 * ).orElseThrow(CEmailSignFailedException::new));
+		 */
 		return null;
+			
+		
 		
 		
 	}
